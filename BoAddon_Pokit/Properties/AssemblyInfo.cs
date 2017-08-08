fa3734b384029233801f11787fd68120bed1d8d7
harold.duan <duanhan0920@hotmail.com>
2017-08-08 13:33:29 +0800

    Update the publish copyright information.

--- BoAddon_Pokit/Properties/AssemblyInfo.cs
+++ BoAddon_Pokit/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AVATECH")]
 [assembly: AssemblyProduct("BoAddon_Pokit")]
-[assembly: AssemblyCopyright("Copyright © AVATECH  2011")]
+[assembly: AssemblyCopyright("Copyright © AVATECH  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
