255e5dbbfea38bf901af4b0ee2c2c77bb9dae83e
harold.duan <duanhan0920@hotmail.com>
2017-08-11 10:19:48 +0800

    Upgrade the README.ps1,add the grip check.

--- BoAddonBiz_Pokit/ReadMe/README.ps1
+++ BoAddonBiz_Pokit/ReadMe/README.ps1
@@ -4,6 +4,7 @@ echo ***************************************************************************
 echo "Generte the README.html."
 echo "Starting..."
 echo "This current path is " """$PSScriptRoot"""
+
 try
 {
     echo "This current pip version:"
@@ -15,6 +16,18 @@ catch
     Write-Host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
     Write-Host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red
     echo "Please install python & pip and add the python to path."
+}
+try
+{
+    echo "This current grip version:"
+    grip "-V"
+}
+catch
+{
+    Write-Host "Caught an exception:" -ForegroundColor Red
+    Write-Host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
+    Write-Host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red
+    echo "Please install grip by pip."
 }
 
 echo "Exporting..."
